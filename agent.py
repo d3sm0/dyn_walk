56dc9e1bc3cc89aa774751e24fcc47f954e3607b
d3sm0 <simone.totaro@gmail.com>
2017-08-26 12:20:38 +0200

    added wrapper, priortized memory replay, cleaned up

--- agent.py
+++ agent.py
@@ -1,63 +1,61 @@
 import tensorflow as tf
 from ddpg import Actor , Critic , build_summaries
 import numpy as np
-from replay_buffer import ReplayBuffer
+from memory.pmr import Experience
 
 
-class Agent ( object ):
-    def __init__(self , name , env_dim , target=None , writer=None , h_size=128 , memory_size=10e6, stochastic = False):
+class Agent( object ):
+    def __init__(self , name , env_dims , target=None , writer=None , h_size=128 , batch_size=32 , memory_size=10e6 ,
+                 policy='det' ,
+                 act=tf.nn.elu , split_obs=None):
 
-        obs_space , act_space , bound = env_dim
-        with tf.variable_scope ( name ):
-            self.actor = Actor ( obs_space , act_space , bound , h_size=h_size, stochastic=stochastic)
-            self.critic = Critic ( obs_space , act_space , h_size=h_size )
+        self.obs_space , self.act_space , bound = env_dims
+        with tf.variable_scope( name ):
+            self.actor = Actor( self.obs_space , self.act_space , bound , h_size=h_size , policy=policy , act=act ,
+                                split_obs=split_obs )
+            self.critic = Critic( self.obs_space , self.act_space , h_size=h_size , act=act )
 
-        # TODO Use share memory between worker
         # TODO test prioritized experience replay
-        self.memory = ReplayBuffer ( env_shape=env_dim , buffer_size=memory_size )
+        # self.memory = Experience ( batch_size=batch_size, buffer_size=int(memory_size))
+        self.memory = Experience( batch_size=batch_size , memory_size=int( memory_size ) )
 
         if name != 'target':
+            self.sync_op = [ self.update_target( self.actor.params , target.actor.params ) ,
+                             self.update_target( self.critic.params , target.critic.params ) ]
 
-            self.sync_op = [ self.update_target ( self.actor.params , target.actor.params ) ,
-                             self.update_target ( self.critic.params , target.critic.params ) ]
-
-
-            self.summary_ops = build_summaries ( scalar=[ self.critic.q , self.critic.critic_loss ] ,
-                                                 hist=[ self.critic.state , self.critic.action ,
-                                                        self.actor.grads ] )
+            self.summary_ops = build_summaries( scalar=[ self.critic.q , self.critic.critic_loss ] ,
+                                                hist=[ self.critic.state , self.critic.action ,
+                                                       self.actor.grads ] )
 
             self.writer = writer
 
         self.name = name
         self.target = target
-        self.obs_space = env_dim[ 0 ]
-        self.act_space = env_dim[ 1 ]
-        self.h_size = h_size
+        self.gamma = 0.99
 
-        tf.logging.info('Worker {} ready to go ...'.format(self.name))
+        tf.logging.info( 'Worker {} ready to go ...'.format( self.name ) )
 
     def summarize(self , feed_dict , global_step):
 
-        sess = tf.get_default_session ()
+        sess = tf.get_default_session()
 
-        summary = sess.run ( self.summary_ops ,
-                             feed_dict=feed_dict )
+        summary = sess.run( self.summary_ops ,
+                            feed_dict=feed_dict )
 
-        self.writer.add_summary ( summary , global_step=global_step )
-        self.writer.flush ()
+        self.writer.add_summary( summary , global_step=global_step )
+        self.writer.flush()
 
     def sync(self):
-        # tf.logging.info('Updating target network')
-        sess = tf.get_default_session ()
-        sess.run ( self.sync_op )
 
+        sess = tf.get_default_session()
+        sess.run( self.sync_op )
 
     def train(self , state , action , q , get_summary=False):
 
-        sess = tf.get_default_session ()
+        sess = tf.get_default_session()
 
-        critic_loss , _ , global_step = sess.run (
-            [ self.critic.critic_loss , self.critic.train , tf.contrib.framework.get_global_step () ] , feed_dict={
+        critic_loss , _ , global_step = sess.run(
+            [ self.critic.critic_loss , self.critic.train , tf.contrib.framework.get_global_step() ] , feed_dict={
                 self.critic.state: state ,
                 self.critic.action: action ,
                 self.critic.q: q
@@ -65,10 +63,10 @@ class Agent ( object ):
 
         # compute sample of the gradient
 
-        sampled_action = self.get_action ( state )
-        sampled_grads = self.get_grads ( state , sampled_action )
+        sampled_action = self.get_action( state )
+        sampled_grads = self.get_grads( state , sampled_action )
 
-        _ , = sess.run ( [ self.actor.train ] , feed_dict={
+        _ , = sess.run( [ self.actor.train ] , feed_dict={
             self.actor.state: state ,
             self.actor.grads: sampled_grads
         } )
@@ -81,74 +79,116 @@ class Agent ( object ):
                 self.critic.action: action ,
                 self.critic.q: q
             }
-            self.summarize ( feed_dict , global_step )
+            self.summarize( feed_dict , global_step )
 
         return critic_loss
 
-
     def get_grads(self , state , action):
-        sess = tf.get_default_session ()
-        return sess.run ( self.critic.action_grads , feed_dict={
+        sess = tf.get_default_session()
+        return sess.run( self.critic.action_grads , feed_dict={
             self.critic.state: state ,
             self.critic.action: action ,
         } )[ 0 ]
 
     def get_action(self , state):
-        sess = tf.get_default_session ()
+        sess = tf.get_default_session()
 
         # TODO pre-process of state should not happen here
-        if np.ndim ( state ) != self.obs_space:
-            state = np.reshape ( state , (-1 , self.obs_space) )
+        if np.ndim( state ) != self.obs_space:
+            state = np.reshape( state , (-1 , self.obs_space) )
 
-        mu_hat = sess.run ( self.actor.mu_hat ,
-                            feed_dict={self.actor.state: state} )
-
+        mu_hat = sess.run( self.actor.mu_hat ,
+                           feed_dict={self.actor.state: state} )
         return mu_hat
 
     def get_q(self , state , action):
 
-        sess = tf.get_default_session ()
-        if np.ndim ( state ) != self.obs_space:
-            state = np.reshape ( state , (-1 , self.obs_space) )
+        sess = tf.get_default_session()
+        if np.ndim( state ) != self.obs_space or np.ndim( action ) != self.act_space:
+            state = np.reshape( state , (-1 , self.obs_space) )
+            action = np.reshape( action , (-1 , self.act_space) )
 
-        q_hat = sess.run ( self.critic.q_hat , feed_dict={self.critic.state: state , self.critic.action: action} )
-        return q_hat
+        q_hat = sess.run( self.critic.q_hat , feed_dict={self.critic.state: state , self.critic.action: action} )
+        return q_hat.ravel()
 
-    def think(self , gamma , batch_size , summarize):
+    # def think(self, summarize):
+    #
+    #     if self.memory.get_size() > self.memory.batch_size:
+    #         s1_batch , a_batch , r_batch , s2_batch , t_batch = self.memory.select ( )
+    #
+    #         target_action = self.target.get_action ( s2_batch )
+    #         target_q = self.target.get_q ( s2_batch , target_action )
+    #
+    #         y_i = [ ]
+    #
+    #         for k in range ( self.memory.batch_size ):
+    #             if t_batch[ k ]:
+    #                 y_i.append ( r_batch[ k ] )
+    #             else:
+    #                 y_i.append ( r_batch[ k ] + self.gamma * target_q[ k ] )
+    #
+    #         y_i = np.reshape ( y_i , (self.memory.batch_size , 1) )
+    #
+    #         c_loss = self.train ( s1_batch , a_batch , y_i , get_summary=summarize )
 
-        if self.memory.get_size () > batch_size:
-
-            s1_batch , a_batch , r_batch , s2_batch , t_batch = self.memory.get_sample ( batch_size )
 
 
-            target_action = self.target.get_action ( s2_batch )
-            target_q = self.target.get_q ( s2_batch , target_action )
+    def get_td(self , state , action , reward , next_state , terminal):
 
-            y_i = [ ]
+        target_action = self.target.get_action( next_state )
 
-            for k in range ( batch_size ):
+        target_q = self.target.get_q( next_state , target_action )[0]
+        local_q = self.get_q( state , action )[0]
+        q = reward
+
+        if not terminal:
+            q += self.gamma * target_q
+
+        return np.abs( q - local_q ), q
+
+    def think(self , summarize):
+
+        data , _ , idxs = self.memory.select()
+
+        if data is not None:
+
+            s1_batch , a_batch , r_batch , s2_batch , t_batch = self.memory.prepare_output( data )
+
+            target_action = self.target.get_action( s2_batch )
+            target_q = self.target.get_q( s2_batch , target_action )
+
+            local_q = self.get_q( s1_batch , a_batch )
+            # TODO check data structure here
+            q = [ ]
+            td = [ ]
+            for k in range( self.memory.batch_size ):
                 if t_batch[ k ]:
-                    y_i.append ( r_batch[ k ] )
+                    q.append( r_batch[ k ] )
+                    td.append( r_batch[ k ] )
                 else:
-                    y_i.append ( r_batch[ k ] + gamma * target_q[ k ] )
+                    q.append( r_batch[ k ] + self.gamma * target_q[ k ] )
+                    td.append( r_batch[ k ] + self.gamma * target_q[ k ] - local_q[ k ] )
 
-            y_i = np.reshape ( y_i , (batch_size , 1) )
+            self.memory.priority_update( indices=idxs , priorities=np.abs( td ) )
 
-            c_loss = self.train ( s1_batch , a_batch , y_i , get_summary=summarize )
+            q = np.reshape( q , (self.memory.batch_size , 1) )
+
+            self.train( s1_batch , a_batch , q , get_summary=summarize )
 
     @staticmethod
     # tau = 0.001
     def update_target(local , target , tau=0.01):
         params = [ ]
-        for i in range ( len ( target ) ):
-            params.append (
-                target[ i ].assign ( tf.multiply ( local[ i ] , tau ) + tf.multiply ( target[ i ] , 1. - tau ) ) )
+        for i in range( len( target ) ):
+            params.append(
+                target[ i ].assign( tf.multiply( local[ i ] , tau ) + tf.multiply( target[ i ] , 1. - tau ) ) )
 
         return params
 
     @staticmethod
     def global_trainer(grads , params):
         grads = [ grad for grad in grads if grad is not None ]
-        grads,_ = tf.clip_by_global_norm(grads, 40)
-        train_op = tf.train.AdamOptimizer ().apply_gradients ( zip ( grads , params ), global_step=tf.contrib.framework.get_global_step ())
+        grads , _ = tf.clip_by_global_norm( grads , 40 )
+        train_op = tf.train.AdamOptimizer().apply_gradients( zip( grads , params ) ,
+                                                             global_step=tf.contrib.framework.get_global_step() )
         return train_op
--- train.py
+++ train.py
@@ -1,88 +1,156 @@
 import tensorflow as tf
 
-from ou_noise import OUNoise
+from datetime import datetime
+from agent import Agent
+from utils.ou_noise import OUNoise
+from utils.env_wrapper import EnvWrapper
+from controller.daddy import Daddy
+import os
+from ddpg import lrelu
 
-tf.logging.set_verbosity ( tf.logging.INFO )
+tf.logging.set_verbosity( tf.logging.INFO )
 
-class Worker ( object ):
-    def __init__(self , env , agent , max_steps=None, batch_size = 64, gamma = 0.99):
-        self.agent = agent
-        self.env = env
+# 'Walker2d-v1'
+# 'Pendulum-v0'
 
-        # self.ou = OUNoise ( action_dimension=agent.act_space )
+# TODO create argument parser
+# TODO create faster way to write experiment readme
+# TODO save dataset of experience !
+# TODO check prioritized experience code
+# TODO rewrite wrapper as FIFO or queue
 
-        self.t = 0
-        self.max_steps = max_steps
-        self.batch_size = batch_size
-        self.gamma = gamma
+ENV_NAME = 'osim'
+MEMORY_SIZE = int(1e6)
+BATCH_SIZE = 32
+GAMMA = 0.99
+NUM_EP = 5000
+SAVE_EVERY = 100
+H_SIZE = [ 128, 64]
+PRE_TRAIN = None
+POLICY = 'det' # stochastic, sin
+ACTIVATION = lrelu
+CONCATENATE_FRAMES  = 3
+USE_RW = True
 
-    def run(self , sess , coord):
+NORMALIZE = True
+DESCRIPTON = 'Testing osim concatenatig 3 frames with with augmented reward and time. Using prioritzed memory and 128,64 as size of hidden layer'
 
+def main():
+    now = datetime.utcnow().strftime( "%b-%d_%H_%M" )  # create unique dir
+    full_path = os.path.join( os.getcwd() , 'logs' , now )
 
-        with sess.as_default () , sess.graph.as_default ():
+    if ENV_NAME == 'osim':
 
-            last_t = 0
-            summarize = False
+        from osim.env import RunEnv
 
+        try:
+            env = EnvWrapper(RunEnv, augment_rw=USE_RW, add_time=True, concat=CONCATENATE_FRAMES, normalize=NORMALIZE, add_acceleration = 5)
+            env_dims = env.get_dims()
+        except:
+            tf.logging.info('Environment not found')
+    else:
+        try:
+            import gym
+            env = gym.make(ENV_NAME)
+            env_dims = (env.observation_space.shape[0], env.action_space.shape[0], (env.action_space.low, env.action_space.high))
+        except:
+            raise NotImplementedError
 
+    ou = OUNoise( action_dimension=env_dims[1])
+
+    target = Agent( name='target' , env_dims=env_dims , h_size=H_SIZE , policy = POLICY, act=ACTIVATION, split_obs = None) #env sep
+
+    global_step = tf.Variable( 0 , trainable=False , name='global_step' )
+    writer = tf.summary.FileWriter( full_path )
+
+    with open(os.path.join(full_path,'readme.md'), 'w+') as f:
+        f.write(DESCRIPTON)
+
+    saver = tf.train.Saver( tf.get_collection( tf.GraphKeys.GLOBAL_VARIABLES ) , max_to_keep=2 )
+    ckpt = tf.train.latest_checkpoint( full_path )
+
+    agent = Agent( name='local' , env_dims=env_dims , target=target , writer=writer , h_size=H_SIZE ,
+                   policy=POLICY, act=ACTIVATION, split_obs=None)
+
+    if PRE_TRAIN is not None:
+        daddy = Daddy( target=agent , env_dim=env_dims)
+
+    with tf.Session() as sess:
+
+        if ckpt:
             try:
-                while not coord.should_stop ():
+                tf.logging.info( 'Restore model {}'.format( ckpt ) )
+                saver.restore( sess=sess , save_path=ckpt )
+            except Exception as e:
+                tf.logging.info(e)
+
+        sess.run( tf.global_variables_initializer() )
+
+        if PRE_TRAIN is not None:
+            daddy.experience(env = env,epsiodes=PRE_TRAIN, writer=agent.writer)
+            saver.save( sess , os.path.join( full_path , 'pre_train', 'model.ckpt' ) , global_step=PRE_TRAIN )
+            tf.logging.info('Pre-train ended, starting training now...')
+
+        summarize = False
+        ep_summary = tf.Summary()
+
+        for ep in range( NUM_EP ):
+
+            agent.sync()
+            state = env.reset()
+            ou.reset()
+
+            terminal = False
+            timesteps , tot_rw , tot_q , tot_td = 0 , 0 , 0 , 0
+
+            # z_filter = ZFilter( (1 , env_dims[ 0 ]) )
+            # state = z_filter( unscaled_state )
+
+            while not terminal:
+                # if stochastic remove exploration noise
+
+                action = agent.get_action( state ).flatten() + ou.noise()
+                next_state , reward , terminal , _ = env.step( action.flatten() )
+
+                td , q = agent.get_td( state , action , reward , next_state , terminal )
+                agent.memory.add( (state , action , reward , next_state, terminal) , priority=td )
+                # agent.memory.add(state, action, reward, next_state, terminal)
 
 
-                    self.state = self.env.reset ()
-                    # self.noise = self.ou.reset()
-                    self.agent.sync ()
+                agent.think(summarize=summarize )
 
-                    timesteps,tot_rw= self.sample (summarize=summarize)
-                    summarize = False
+                state = next_state
+                summarize = False
+                timesteps += 1
 
-                    self.t += 1
+                tot_td += td
+                tot_q += q
+                # tot_rw += reward
 
-                    if self.agent.name == 'worker_0' and self.t - last_t >5:
-                        last_t = self.t
-                        summarize = True
+            if ep % 5 == 0:
 
-                        tf.logging.info (
-                            'Master ep  {}, latest ep reward {}, of steps {}'.format ( self.t , tot_rw , timesteps ) )
+                summarize = True
+                # use only the true reward of the environment
+                tot_rw = env.r
+                ep_summary.value.add( simple_value=tot_rw / timesteps , tag='eval/avg_rw' )
+                ep_summary.value.add( simple_value=tot_q / timesteps , tag='eval/avg_q' )
+                ep_summary.value.add( simple_value=tot_td / timesteps , tag='eval/avg_td' )
+                ep_summary.value.add( simple_value=tot_rw , tag='eval/tot_rw' )
+                ep_summary.value.add( simple_value=tot_td , tag='eval/total_td' )
+                ep_summary.value.add( simple_value=tot_q , tag='eval/total_q' )
+                ep_summary.value.add( simple_value=timesteps , tag='eval/ep_length' )
 
-                    if self.max_steps is not None and self.t > self.max_steps:
-                        tf.logging.info ( 'Hopefully i learnt something...test me...' )
-                        coord.should_stop ()
+                agent.writer.add_summary( ep_summary , ep )
+                agent.writer.flush()
 
-            except tf.errors.CancelledError:
-                return
+                tf.logging.info(
+                    'Master ep  {}, latest avg reward {}, of steps {}'.format( ep , tot_rw / timesteps , timesteps ) )
 
-    def sample(self, summarize = False):
+            if ep % SAVE_EVERY == 0:
+                gs = tf.train.global_step( sess , global_step )
+                saver.save( sess , os.path.join( full_path , 'model.ckpt' ) , global_step=gs )
+                tf.logging.info( 'Model saved at ep {}'.format( gs ) )
 
-        terminal = False
 
-        t , tot_rw = 0 , 0
-        # f_in = self.agent.reset()
-
-        while not terminal:
-            # if stochastic remove exploration noise
-
-            action = self.agent.get_action ( self.state )
-            # action = action + self.ou.noise()
-
-            next_state , reward , terminal , _ = self.env.step ( action)
-            self.agent.memory.collect ( self.state , action , reward , next_state , terminal )
-            self.agent.think (batch_size=self.batch_size, gamma = self.gamma, summarize  = summarize)
-            self.state = next_state
-            t += 1
-            tot_rw += reward
-
-            if terminal:
-                break
-
-        if summarize:
-
-            ep_summary = tf.Summary ()
-
-            ep_summary.value.add ( simple_value=tot_rw , tag='eval/total_rw' )
-            ep_summary.value.add ( simple_value=t, tag='eval/ep_length' )
-
-            self.agent.writer.add_summary ( ep_summary , self.t )
-            self.agent.writer.flush ()
-
-        return t , tot_rw
+if __name__ == '__main__':
+    main()
