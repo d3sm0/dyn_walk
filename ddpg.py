76bd390c5c8461cf60e86c5f768c3cce5ae2f392
d3sm0 <simone.totaro@gmail.com>
2017-08-30 22:46:18 +0200

    first commit

--- ddpg.py
+++ ddpg.py
@@ -1,160 +1,162 @@
 import tensorflow as tf
-from tensorflow.contrib.layers import fully_connected , summarize_activation , summarize_tensor
+from tensorflow.contrib.layers import fully_connected, summarize_activation, summarize_tensor
 
 
-class Actor( object ):
-    def __init__(self , obs_space , action_space , action_bound , h_size , lr=1e-4 , act=tf.nn.relu , policy='det' ,
+class Actor(object):
+    def __init__(self, obs_space, action_space, action_bound, h_size, lr=1e-4, act=tf.nn.relu, policy='det',
                  split_obs=None):
-        self.state = tf.placeholder( 'float32' , shape=[ None , obs_space ] , name='state' )
+        self.state = tf.placeholder('float32', shape=[None, obs_space], name='state')
 
         if split_obs is not None:
-            self.internal_state = self.state[ : , :split_obs ]
-            self.external_state = self.state[ : , split_obs: ]
+            self.internal_state = self.state[:, :split_obs]
+            self.external_state = self.state[:, split_obs:]
 
-        self.grads = tf.placeholder( 'float32' , shape=[ None , action_space ] , name='gradients' )
+        self.grads = tf.placeholder('float32', shape=[None, action_space], name='gradients')
 
-        self.mu_hat = self.build_network( h_size , action_space , action_bound , act , policy ,
-                                          has_external=bool( split_obs ) )
+        self.mu_hat = self.build_network(h_size, action_space, action_bound, act, policy,
+                                         has_external=bool(split_obs))
 
-        self.params = tf.get_collection( tf.GraphKeys.GLOBAL_VARIABLES , scope=tf.get_variable_scope().name )
+        self.params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=tf.get_variable_scope().name)
 
-        self.actor_grads = tf.gradients( self.mu_hat , self.params , -self.grads )
+        self.actor_grads = tf.gradients(self.mu_hat, self.params, -self.grads)
 
-        self.train = tf.train.AdamOptimizer( learning_rate=lr ).apply_gradients( zip( self.actor_grads , self.params ) ,
-                                                                                 global_step=tf.contrib.framework.get_global_step() )
+        self.train = tf.train.AdamOptimizer(learning_rate=lr).apply_gradients(zip(self.actor_grads, self.params),
+                                                                              global_step=tf.contrib.framework.get_global_step())
 
-    def build_network(self , h_size , action_space , action_bound , act , policy , has_external=True):
+    def build_network(self, h_size, action_space, action_bound, act, policy, has_external=True):
 
         if has_external:
 
-            p0 = fully_connected( inputs=self.internal_state , num_outputs=h_size[ 0 ] , activation_fn=act )
+            p0 = fully_connected(inputs=self.internal_state, num_outputs=h_size[0], activation_fn=act)
 
-            p1 = fully_connected( inputs=self.external_state , num_outputs=h_size[ 0 ] , activation_fn=act )
+            p1 = fully_connected(inputs=self.external_state, num_outputs=h_size[0], activation_fn=act)
 
-            p1 = fully_connected( inputs=p1 , num_outputs=h_size[ 0 ] , activation_fn=act )
-            h1 = fully_connected( inputs=p0 , num_outputs=h_size[ 1 ] , activation_fn=act )
-            h2 = fully_connected( inputs=h1 , num_outputs=h_size[ 1 ] , activation_fn=act )
+            p1 = fully_connected(inputs=p1, num_outputs=h_size[0], activation_fn=act)
+            h1 = fully_connected(inputs=p0, num_outputs=h_size[1], activation_fn=act)
+            h2 = fully_connected(inputs=h1, num_outputs=h_size[1], activation_fn=act)
 
-            h2 = tf.concat( (h2 , p1) , axis=1 )
+            h2 = tf.concat((h2, p1), axis=1)
 
-            summarize_activation( p0 )
-            summarize_activation( p1 )
+            summarize_activation(p0)
+            summarize_activation(p1)
 
         else:
-            h1 = fully_connected( inputs=self.state , num_outputs=h_size[ 0 ] , activation_fn=act )
-            h2 = fully_connected( inputs=h1 , num_outputs=h_size[ 1 ] , activation_fn=act )
+            h1 = fully_connected(inputs=self.state, num_outputs=h_size[0], activation_fn=act)
+            h2 = fully_connected(inputs=h1, num_outputs=h_size[1], activation_fn=act)
 
-        mu_hat = fully_connected( inputs=h2 , num_outputs=action_space , activation_fn=tf.nn.tanh ,
-                                  weights_initializer=tf.random_uniform_initializer( minval=-0.003 , maxval=0.003 ) ,
-                                  scope='policy' )
+        mu_hat = fully_connected(inputs=h2, num_outputs=action_space, activation_fn=tf.nn.tanh,
+                                 weights_initializer=tf.random_uniform_initializer(minval=-0.003, maxval=0.003),
+                                 scope='policy')
 
         if policy == 'stochastic':
-            mu_hat = self.stochastic_policy( h2, mu_hat , action_space )
+            mu_hat = self.stochastic_policy(h2, mu_hat, action_space)
 
         elif policy == 'sin':
-            mu_hat = self.sin_policy( h2 , mu_hat , action_space )
+            mu_hat = self.sin_policy(h2, mu_hat, action_space)
 
-        action = tf.clip_by_value( mu_hat , action_bound[0],action_bound[1] , name='scaled' )
+        action = tf.clip_by_value(mu_hat, action_bound[0], action_bound[1], name='scaled')
 
-        summarize_activation( h1 )
-        summarize_activation( h2 )
+        summarize_activation(h1)
+        summarize_activation(h2)
 
-        summarize_activation( mu_hat )
+        summarize_activation(mu_hat)
 
         return action
 
-    def sin_policy(self , h2 , mu_hat , action_space):
+    def sin_policy(self, h2, mu_hat, action_space):
 
-        amplitude = fully_connected( inputs=h2 , num_outputs=action_space , activation_fn=None )
+        amplitude = fully_connected(inputs=h2, num_outputs=action_space, activation_fn=None)
 
-        mu_hat = tf.map_fn( fn=lambda x: tf.sin( x * 2 ) + 1 / 2 , elems=mu_hat )
-        mu_hat = tf.multiply( amplitude , mu_hat )
+        mu_hat = tf.map_fn(fn=lambda x: tf.sin(x * 2) + 1 / 2, elems=mu_hat)
+        mu_hat = tf.multiply(amplitude, mu_hat)
 
         return mu_hat
 
-    def stochastic_policy(self , h2, mu_hat , action_space):
+    def stochastic_policy(self, h2, mu_hat, action_space):
 
-        var = fully_connected( inputs=h2 , num_outputs=action_space , activation_fn=tf.nn.softplus )
+        var = fully_connected(inputs=h2, num_outputs=action_space, activation_fn=tf.nn.softplus)
         #
         # var = tf.nn.softplus( tf.get_variable( 'sd' , dtype=tf.float32 , shape=(action_space ,) ) )
         # var = tf.reshape( var , (-1 , action_space) )
 
-        action = mu_hat + tf.random_normal( shape=tf.shape( mu_hat ) ) * tf.sqrt( var )
-        summarize_tensor( var , tag='var' )
-        summarize_tensor( action , tag='sampled_action' )
+        action = mu_hat + tf.random_normal(shape=tf.shape(mu_hat)) * tf.sqrt(var)
+        summarize_tensor(var, tag='var')
+        summarize_tensor(action, tag='sampled_action')
         return action
 
 
-class Critic( object ):
-    def __init__(self , obs_space , action_space , h_size , lr=1e-3 , act=tf.nn.relu , split_obs=None):
-        self.state = tf.placeholder( 'float32' , shape=[ None , obs_space ] , name='state' )
-        self.action = tf.placeholder( 'float32' , shape=[ None , action_space ] , name='action' )
+class Critic(object):
+    def __init__(self, obs_space, action_space, h_size, lr=1e-3, act=tf.nn.relu, split_obs=None):
+        self.state = tf.placeholder('float32', shape=[None, obs_space], name='state')
+        self.action = tf.placeholder('float32', shape=[None, action_space], name='action')
 
-        self.q = tf.placeholder( 'float32' , shape=[ None , 1 ] , name='q' )
+        self.q = tf.placeholder('float32', shape=[None, 1], name='q')
 
         if split_obs is not None:
-            self.internal_state = self.state[ : , :split_obs ]
-            self.external_state = self.state[ : , split_obs: ]
+            self.internal_state = self.state[:, :split_obs]
+            self.external_state = self.state[:, split_obs:]
 
-        self.q_hat = self.build_network( h_size , action_space , act , has_external=bool( split_obs ) )
+        self.q_hat = self.build_network(h_size, action_space, act, has_external=bool(split_obs))
 
-        self.params = tf.get_collection( tf.GraphKeys.GLOBAL_VARIABLES , scope=tf.get_variable_scope().name )
+        self.params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=tf.get_variable_scope().name)
 
-        self.critic_loss = tf.reduce_mean( tf.squared_difference( self.q , self.q_hat ) , name='critic_loss' )
+        self.critic_loss = tf.reduce_mean(tf.squared_difference(self.q, self.q_hat), name='critic_loss')
 
-        self.train = tf.train.AdamOptimizer( learning_rate=lr ).minimize( self.critic_loss ,
-                                                                          global_step=tf.contrib.framework.get_global_step() )
+        # self.critic_gradients = tf.gradients(self.critic_loss, self.params)
 
-        self.action_grads = tf.gradients( self.q_hat , self.action )
+        self.train = tf.train.AdamOptimizer(learning_rate=lr).minimize(self.critic_loss,
+                                                                       global_step=tf.contrib.framework.get_global_step())
 
-    def build_network(self , h_size , action_space , act , has_external):
+        self.action_grads = tf.gradients(self.q_hat, self.action)
+
+    def build_network(self, h_size, action_space, act, has_external):
 
         if has_external:
 
-            p0 = fully_connected( inputs=self.internal_state , num_outputs=h_size[ 0 ] , activation_fn=act )
-            p1 = fully_connected( inputs=self.external_state , num_outputs=h_size[ 0 ] , activation_fn=act )
+            p0 = fully_connected(inputs=self.internal_state, num_outputs=h_size[0], activation_fn=act)
+            p1 = fully_connected(inputs=self.external_state, num_outputs=h_size[0], activation_fn=act)
 
-            p1 = fully_connected( inputs=p1 , num_outputs=h_size[ 0 ] , activation_fn=act )
+            p1 = fully_connected(inputs=p1, num_outputs=h_size[0], activation_fn=act)
 
-            h1 = fully_connected( inputs=p0 , num_outputs=h_size[ 0 ] , activation_fn=act )
+            h1 = fully_connected(inputs=p0, num_outputs=h_size[0], activation_fn=act)
 
-            h1 = tf.concat( (h1 , p1) , axis=1 )
+            h1 = tf.concat((h1, p1), axis=1)
 
-            summarize_activation( p0 )
-            summarize_activation( p1 )
+            summarize_activation(p0)
+            summarize_activation(p1)
 
         else:
-            h1 = fully_connected( inputs=self.state , num_outputs=h_size[ 0 ] , activation_fn=act )
+            h1 = fully_connected(inputs=self.state, num_outputs=h_size[0], activation_fn=act)
 
-        w1 = tf.get_variable( 'w1' , shape=[ h_size[ 0 ] , h_size[ 1 ] ] , dtype=tf.float32 )
-        w2 = tf.get_variable( 'w2' , shape=[ action_space , h_size[ 1 ] ] , dtype=tf.float32 )
-        b2 = tf.get_variable( 'b2' , shape=[ h_size[ 1 ] ] , dtype=tf.float32 )
+        w1 = tf.get_variable('w1', shape=[h_size[0], h_size[1]], dtype=tf.float32)
+        w2 = tf.get_variable('w2', shape=[action_space, h_size[1]], dtype=tf.float32)
+        b2 = tf.get_variable('b2', shape=[h_size[1]], dtype=tf.float32)
 
-        h2 = act( tf.matmul( h1 , w1 ) + tf.matmul( self.action , w2 ) + b2 )
+        h2 = act(tf.matmul(h1, w1) + tf.matmul(self.action, w2) + b2)
 
-        q_hat = fully_connected( inputs=h2 , num_outputs=1 , activation_fn=None ,
-                                 weights_initializer=tf.random_uniform_initializer( minval=-0.003 , maxval=0.003 ) ,
-                                 scope='value' )
+        q_hat = fully_connected(inputs=h2, num_outputs=1, activation_fn=None,
+                                weights_initializer=tf.random_uniform_initializer(minval=-0.003, maxval=0.003),
+                                scope='value')
 
-        summarize_activation( h2 )
-        summarize_activation( q_hat )
+        summarize_activation(h2)
+        summarize_activation(q_hat)
 
         return q_hat
 
 
-def build_summaries(scalar=None , hist=None):
+def build_summaries(scalar=None, hist=None):
     for v in scalar:
-        tf.summary.scalar( v.name.replace( ':' , "_" ) + 'mean' , tf.reduce_mean( v ) )
+        tf.summary.scalar(v.name.replace(':', "_") + 'mean', tf.reduce_mean(v))
 
     if hist is not None:
         for h in hist:
-            tf.summary.histogram( h.name.replace( ':' , "_" ) , h )
+            tf.summary.histogram(h.name.replace(':', "_"), h)
 
-    summary_ops = tf.get_collection( tf.GraphKeys.SUMMARIES , scope=tf.get_variable_scope().name )
-    summary_ops = [ s for s in summary_ops if 'target' not in s.name ]
+    summary_ops = tf.get_collection(tf.GraphKeys.SUMMARIES, scope=tf.get_variable_scope().name)
+    summary_ops = [s for s in summary_ops if 'target' not in s.name]
 
-    return tf.summary.merge( summary_ops )
+    return tf.summary.merge(summary_ops)
 
 
-def lrelu(x , alpha=0.05, name = None):
-    return tf.subtract(tf.nn.relu( x ), alpha * tf.nn.relu( -x ), name = name)
+def lrelu(x, alpha=0.05, name=None):
+    return tf.subtract(tf.nn.relu(x), alpha * tf.nn.relu(-x), name=name)
--- train.py
+++ train.py
@@ -1,19 +1,21 @@
 import tensorflow as tf
+from agent import Worker, Agent, Target
+
 from datetime import datetime
-from agent import Agent
 import os
 import numpy as np
 
-from utils.ou_noise import OUNoise
+from agent import Agent
 from utils.env_wrapper import EnvWrapper
-from controller.daddy import Daddy
+
 from ddpg import lrelu
+from motivation.icm import ICM
+import argparse
+import json
 
-# from motivation.icm import  ICM
-# from scale_state import ZFilter
 
+tf.logging.set_verbosity(tf.logging.INFO)
 
-tf.logging.set_verbosity( tf.logging.INFO )
 
 # TODO create argument parser
 # TODO create faster way to write experiment readme
@@ -21,260 +23,44 @@ tf.logging.set_verbosity( tf.logging.INFO )
 # TODO check prioritized experience code
 # TODO rewrite wrapper as FIFO or queue
 # TODO Check reward function
-# TODO rewrite assisted training
 
-ENV_NAME = 'osim'  # 'Walker2d-v1''Pendulum-v0''osim'
-MEMORY_SIZE = int( 1e6 )
-BATCH_SIZE = 32
-GAMMA = 0.99
-NUM_EP = 5000
-SAVE_EVERY = 100
-H_SIZE = [ 128 , 64 ]
-PRE_TRAIN = None
-POLICY = 'det'  # stochastic, sin
-ACTIVATION = lrelu
-CONCATENATE_FRAMES = 3
-USE_RW = True
-CLIP = 20
-LOAD_FROM = 'Aug-29_07_34'
-NORMALIZE = True
-FRAME_RATE = 50
-DESCRIPTON = """
-Experimenting with assisted policy. Using acceleration, localized metrics, batch normalization, clipping.
-"""
 
-
-def softmax(x):
-    """Compute softmax values for each sets of scores in x."""
-    e_x = np.exp( x - np.max( x ) )
-    return e_x / e_x.sum()
-
-def main():
-    now = datetime.utcnow().strftime( "%b-%d_%H_%M" )  # create unique dir
-
-    full_path = os.path.join( os.getcwd() , ENV_NAME , 'logs' , now )
-
-    if ENV_NAME == 'osim':
-
-        from osim.env import RunEnv
-
-        try:
-            env = EnvWrapper( RunEnv , visualize=False , augment_rw=USE_RW , add_time=False ,
-                              concat=CONCATENATE_FRAMES , normalize = NORMALIZE, add_acceleration=7, frame_rate=FRAME_RATE)
-            split_obs = env.split_obs
-            env_dims = env.get_dims()
-        except:
-            tf.logging.info( 'Environment not found' )
-    else:
-        try:
-            import gym
-            env = gym.make( ENV_NAME )
-            env_dims = (env.observation_space.shape[ 0 ] , env.action_space.shape[ 0 ] ,
-                        (env.action_space.low , env.action_space.high))
-            split_obs = None
-        except:
-            raise NotImplementedError
-
-    global_step = tf.Variable( 0 , trainable=False , name='global_step' )
-    writer = tf.summary.FileWriter( full_path )
-
-    with open( os.path.join( full_path , 'readme.md' ) , 'w+' ) as f:
-        f.write( DESCRIPTON )
-
-    target = Agent( name='target' , env_dims=env_dims , h_size=H_SIZE , policy=POLICY , act=ACTIVATION ,
-                    split_obs=None )
-    agent = Agent( name='local' , env_dims=env_dims , target=target , writer=writer , h_size=H_SIZE ,
-                   policy=POLICY , act=ACTIVATION , split_obs=None )
-
-    saver = tf.train.Saver( tf.get_collection( tf.GraphKeys.GLOBAL_VARIABLES ) , max_to_keep=2 )
-    ckpt = tf.train.latest_checkpoint( full_path )
-
-    daddy = Daddy( target=agent , env_dim=env_dims )
-
-
-    with tf.Session() as sess:
-
-        if ckpt:
-            try:
-                tf.logging.info( 'Restore model {}'.format( ckpt ) )
-                saver.restore( sess=sess , save_path=ckpt )
-            except Exception as e:
-                tf.logging.info( e )
-
-        sess.run( tf.global_variables_initializer() )
-
-        summarize = False
-        ep_summary = tf.Summary()
-
-        ou = OUNoise( action_dimension=env_dims[ 1 ] )
-
-        rw_controller , rw_actor = [ 0 ] , [ 0 ]
-        pi = 0  # probability of using actor policy
-
-        for ep in range( NUM_EP ):
-
-            agent.sync()
-            state = env.reset()
-            ou.reset()
-
-            # terminal = False
-            timesteps , tot_rw , tot_q , tot_td = 0 , 0 , 0 , 0
-
-            bonus = 0
-
-            n = np.random.rand( 1 )[ 0 ]
-
-            for t in range( env.env.spec.timestep_limit ):
-                # env.render()
-                # if stochastic remove exploration noisew
-                """
-                with p select action from dpg wiht 1-p select action from controller
-
-                """
-                if n < pi:
-
-                    action = agent.get_action( state ).flatten() + ou.noise()
-                else:
-                    action = daddy.get_action(t)
-
-                next_state , reward , terminal , _ = env.step( action )
-
-                td , q = agent.get_td( state , action , reward + bonus , next_state , terminal )
-
-                if CLIP:
-                    reward = np.clip( reward , -CLIP , CLIP )
-
-                agent.memory.add( (state , action , reward + bonus , next_state , terminal) , priority=td )
-
-
-                agent.think( summarize=summarize )
-
-                state = next_state
-                summarize = False
-                timesteps += 1
-
-                tot_td += td
-                tot_q += q
-                tot_rw += reward
-
-                if terminal:
-                    break
-
-            # update running means
-            if n < pi:
-                rw_actor.append( env.r )
-            else:
-                rw_controller.append( env.r )
-
-            rm_controller = np.mean( rw_controller[ :len( rw_controller ) ] )
-            rm_actor = np.mean( rw_actor[ :len( rw_actor ) ] )
-            pi = softmax( np.array( [ rm_actor , rm_controller ] ) )[ 0 ]
-
-            if ep % 5 == 0:
-
-                summarize = True
-
-                tf.logging.info( 'Using {}'.format( 'actor' if n < pi else 'controller' ) )
-
-                ep_summary.value.add( simple_value=tot_rw / timesteps , tag='eval/avg_surr_rw' )
-                ep_summary.value.add( simple_value=tot_q / timesteps , tag='eval/avg_q' )
-                ep_summary.value.add( simple_value=tot_td / timesteps , tag='eval/avg_td' )
-                ep_summary.value.add( simple_value=tot_rw , tag='eval/surr_rw' )
-                ep_summary.value.add( simple_value=tot_td , tag='eval/total_td' )
-                ep_summary.value.add( simple_value=tot_q , tag='eval/total_q' )
-                ep_summary.value.add( simple_value=timesteps , tag='eval/ep_length' )
-
-                if ENV_NAME == 'osim':
-                    ep_summary.value.add( simple_value=env.r / timesteps , tag='eval/avg_rw' )
-                    ep_summary.value.add( simple_value=env.r , tag='eval/total_rw' )
-
-                if PRE_TRAIN:
-                    ep_summary.value.add( simple_value=rm_controller, tag='eval/rm_critic' )
-                    ep_summary.value.add( simple_value=rm_actor , tag='eval/rm_actor' )
-
-
-                agent.writer.add_summary( ep_summary , ep )
-                agent.writer.flush()
-
-                tf.logging.info(
-                    'Master ep  {}, latest avg reward {},bonus taken {},of steps {}'.format( ep , tot_rw / timesteps ,
-                                                                                             bonus , timesteps ) )
-
-            if ep % SAVE_EVERY == 0:
-                gs = tf.train.global_step( sess , global_step )
-                saver.save( sess , os.path.join( full_path , 'model.ckpt' ) , global_step=gs )
-                tf.logging.info( 'Model saved at ep {}'.format( gs ) )
-
-
-def eval(path , NUM_EP=10):
-    full_path = os.path.join( os.getcwd() , ENV_NAME , 'logs' , path )
-    if ENV_NAME == 'osim':
-
-        from osim.env import RunEnv
-
-        try:
-            env = EnvWrapper( RunEnv , visualize=True , augment_rw=USE_RW , add_time=False ,
-                              concat=CONCATENATE_FRAMES , normalize = NORMALIZE, add_acceleration=7, frame_rate=FRAME_RATE)
-            split_obs = env.split_obs
-            env_dims = env.get_dims()
-        except:
-            tf.logging.info( 'Environment not found' )
-    else:
-        try:
-            import gym
-            env = gym.make( ENV_NAME )
-            env_dims = (env.observation_space.shape[ 0 ] , env.action_space.shape[ 0 ] ,
-                        (env.action_space.low , env.action_space.high))
-            split_obs = None
-        except:
-            raise NotImplementedError
-
-
-    target = Agent( name='target' , env_dims=env_dims , h_size=H_SIZE , policy=POLICY , act=ACTIVATION ,
-                    split_obs=None , clip=CLIP )
-
-    agent = Agent( name='local' , env_dims=env_dims , target=target, h_size=H_SIZE ,
-                   policy=POLICY , act=ACTIVATION , split_obs=None )
-
-    saver = tf.train.Saver( tf.get_collection( tf.GraphKeys.GLOBAL_VARIABLES ) , max_to_keep=2 )
-    ckpt = tf.train.latest_checkpoint( full_path )
-
-    with tf.Session() as sess:
-
-        if ckpt:
-            try:
-                tf.logging.info( 'Restore model {}'.format( ckpt ) )
-                saver.restore( sess=sess , save_path=ckpt )
-            except Exception as e:
-                return tf.logging.info( e )
-
-        sess.run( tf.global_variables_initializer() )
-
-        for ep in range( NUM_EP ):
-
-            state = env.reset()
-            timesteps , tot_rw , tot_q , tot_td = 0 , 0 , 0 , 0
-
-            terminal = False
-
-            while not terminal:
-
-                if ENV_NAME != 'osim':
-                    env.render()
-
-                action = agent.get_action( state ).flatten()
-                next_state , reward , terminal , _ = env.step( action )
-                state = next_state
-                timesteps += 1
-                tot_rw += reward
-
-            tf.logging.info(
-                'Master ep  {}, latest avg reward {},of steps {}'.format( ep , tot_rw / timesteps , timesteps ) )
+def main(config):
+    now = datetime.utcnow().strftime("%b-%d_%H_%M")  # create unique dir
+    log_dir = os.path.join(os.getcwd(), config['ENV_NAME'], 'logs', now)
+    env, env_dims, split_obs = Worker.init_environment(config)
     env.close()
+    global_step = tf.Variable(0, trainable=False, name='global_step')
+    try:
+        os.makedirs(log_dir)
+    except:
+        pass
+
+    with open(os.path.join(log_dir, 'readme.md'), 'a') as f:
+        f.write(config['DESCRIPTON'])
+
+    target = Target(log_dir)
+
+    target.initialize(env_dims=env_dims,
+                      h_size=config['H_SIZE'], policy=config['POLICY'], act=eval(config['ACTIVATION']))
+    worker = Worker(target, config, log_dir)
+
+    # worker does stuff, a parallel worker
+    for ep in range(config['NUM_EP']):
+        worker.agent.update_target_network()
+
+        tot_td, tot_q, tot_rw, timesteps = worker.unroll()
+
+        if ep % config['REPORT_EVERY'] == 0:
+            worker.report_metrics(tot_td, tot_q, tot_rw, timesteps, ep)
+
+        if ep % config['SAVE_EVERY'] == 0:
+            worker.agent.save_progress()
+
+        tf.logging.info('Master ep  {}, latest avg reward {}, of steps {}'.format(ep, tot_rw / timesteps, timesteps))
 
 
 if __name__ == '__main__':
-    if LOAD_FROM is not None:
-        eval( LOAD_FROM )
-    else:
-        main()
+    with open("config.json") as fin:
+        kwargs = json.load(fin)
+    main(kwargs)
