e68363e07b1abfe39798c494bcd3f2af39a0a264
d3sm0 <simone.totaro@gmail.com>
2017-09-06 18:58:07 +0200

    'several bug fixed. Now testing hyper params'

--- agent.py
+++ agent.py
@@ -1,24 +1,33 @@
-from value import ValueNetwork
-from policy import PolicyNetwork
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
+
+from policy import PolicyNetwork
+from utils.misc_utils import explained_variance
+from value import ValueNetwork
 
 
 class Agent(object):
-    def __init__(self , obs_dim , act_dim , kl_target=1e-2 , eta=1000 , beta=1.0 , h_size=128):
-        # self.policy_old = PolicyNetwork(name='pi_old' , obs_dim=obs_dim , act_dim=act_dim , eta=eta , h_size=h_size)
+    def __init__(self , obs_dim , act_dim , log_dir , kl_target=1e-1 , eta=1000 , beta=1.0 , h_size=(128 , 64 , 32)):
         self.policy = PolicyNetwork(name='pi' , obs_dim=obs_dim , act_dim=act_dim , eta=eta ,
                                     h_size=h_size , kl_target=kl_target)
-        self.value = ValueNetwork(name='vf' , obs_dim=obs_dim)
+        self.value = ValueNetwork(name='vf' , obs_dim=obs_dim , h_size=h_size)
         self.kl_target = kl_target
         self.beta = beta
         self.lr_multiplier = 1.0
         # self.memory  =Memory(obs_dim, act_dim, max_steps)
         self.sess = tf.Session()
+        self.writer = tf.summary.FileWriter(logdir=log_dir)
+        # self.saver  = tf.train.Saver.save(sess=self.sess, save_path=os.path.join(log_dir,'model.ckpt'))
         self.sess.run(tf.global_variables_initializer())
 
+    def get_action(self , state):
+        return self.sess.run(self.policy.sample , feed_dict={self.policy.obs: [state]}).flatten()
+
+    def get_value(self , state):
+        return self.sess.run(self.value.vf , feed_dict={self.value.obs: state})
+
     def get_action_value(self , state):
-        action , value = self.sess.run([self.policy.pi.sample() , self.value.vf] ,
+        action , value = self.sess.run([self.policy.sample , self.value.vf] ,
                                        feed_dict={self.value.obs: [state] , self.policy.obs: [state]})
         return action.flatten() , value
 
@@ -26,38 +35,47 @@ class Agent(object):
         mu , logstd = self.sess.run([self.policy.mu , self.policy.logstd] , feed_dict={self.policy.obs: state})
         return mu , logstd
 
-    #
-    # def sync(self):
-    #     self.sess.run(self.policy.sync)
-
     def train(self , dataset , num_iter=10 , lr=1e-3):
-        # assert of class dataset
-        # transfer current policy to old policy
-        # self.sync()
-        dataset.data['mu_old'] , logstd_old = self.get_pi(dataset.data['obs'])
 
+        dataset.data['mu_old'] , logstd_old = self.get_pi(dataset.data['obs'])
+        early_stop = 0
         for i in range(num_iter):
+
             for batch in dataset.iterate_once():
                 feed_dict = {self.policy.obs: batch['obs'] , self.policy.adv: batch['adv'] ,
                              self.policy.acts: batch['acts'] , self.policy.mu_old: batch['mu_old'] ,
                              self.policy.logstd_old: logstd_old ,
                              self.policy.beta: self.beta ,
                              self.policy.lr: self.lr_multiplier * lr}
+
                 policy_loss , kl , e , _ = self.sess.run(
-                    [self.policy.loss , self.policy.kl , self.policy.pi.entropy()[0] , self.policy.train] ,
+                    [self.policy.loss , self.policy.kl , self.policy.entropy , self.policy.train] ,
                     feed_dict=feed_dict)
+
             if kl > 4 * self.kl_target:
                 tf.logging.info('KL too high. Stopping update after {}'.format(i))
+                early_stop += 1
                 break
 
         for i in range(num_iter):
             for batch in dataset.iterate_once():
-                feed_dict = {self.value.obs: batch['obs'] , self.value.tdl: batch['tdl']}
+                feed_dict = {self.value.obs: batch['obs'] , self.value.tdl: batch['tdl'] ,
+                             self.value.value_old: batch['vs']}
                 value_loss , _ = self.sess.run([self.value.loss , self.value.train] , feed_dict)
 
         self.update_beta(kl)
 
-        return policy_loss , value_loss , kl , e
+        stats = {
+            '_pl': policy_loss ,
+            '_vl': value_loss ,
+            '_kl': kl ,
+            '_e': e ,
+            '_expl_var': explained_variance(dataset.data['vs'] , dataset.data['tdl']) ,
+            '_beta': self.beta ,
+            '_early_stop': early_stop
+        }
+
+        return stats
 
     # eps (0.7, 3/4)
     def update_beta(self , kl , alpha=1.5 , eps=(2. , 2.)):
@@ -65,13 +83,13 @@ class Agent(object):
         if kl > self.kl_target * eps[0]:
             # increasing penalty coefficient
             self.beta *= alpha
-            tf.logging.info('KL too high, increasing penalty coefficient. Next Beta {}'.format(self.beta))
+            # tf.logging.info('KL too high, increasing penalty coefficient. Next Beta {}'.format(self.beta))
         elif kl < self.kl_target / eps[1]:
             # decrease penalty coeff
             self.beta /= alpha
-            tf.logging.info('KL too low, decreasing penalty coefficient. Next Beta {}'.format(self.beta))
+            # tf.logging.info('KL too low, decreasing penalty coefficient. Next Beta {}'.format(self.beta))
         else:
-            tf.logging.info('We are close enough!')
+            # tf.logging.info('We are close enough!')
             pass
 
     def update_beta_cody(self , kl , alpha=1.5 , eps=(2. , 2.)):
